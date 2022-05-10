37c86c9812ba2c9342c6874d707e97434deb1f96
Billy Taggart <williamtaggart97@gmail.com>
2022-05-10 17:11:48 -0400

    updated model functions to include directions for adding Caching functionality

--- src/models/restaurant.model.ts
+++ src/models/restaurant.model.ts
@@ -1,10 +1,15 @@
 // Contains all direct interactions with the Restaurant Model
 import { pgKnex } from "../configs/db.config";
 import { Restaurant } from "../util/types"
-
+import { redisClient } from "../configs/cache.config";
 
 export const addRestaurant = async (input: Omit<Restaurant, 'id'>): Promise<Restaurant> => {
     try {
+        // Write Ahead Caching
+        // TASK 1: Implement writing to the cache any new restaurants that get added to the backend
+        // Note: We don't expect restaurant meta data to change very often but people may be reading this data often
+        // Question: What type of eviction policy do we think would be best in this situation
+
         const [newRestaurant] = await pgKnex<Restaurant>('Restaurants').insert(input).returning('*');
 
         return newRestaurant;
@@ -16,11 +21,20 @@ export const addRestaurant = async (input: Omit<Restaurant, 'id'>): Promise<Rest
 
 export const getRestaurantById = async (id: string): Promise<Restaurant> => {
     try {
-        return await pgKnex<Restaurant>('Restaurants')
+        // Cache Aside Implementation
+        // STEP 1: Check if Restaurant exists in the cache
+        // write function here
+
+        // STEP 2: If Restaurant is not in the cache, check the db for the record
+        const restaurant: Restaurant = await pgKnex<Restaurant>('Restaurants')
             .leftJoin('Reservations', 'Reservations.restaurantId', 'Restaurants.id')
-            .select('Restaurants.*', pgKnex.raw('JSON_AGG("Reservations".*) as reservations'))
+            .first('Restaurants.*', pgKnex.raw('JSON_AGG("Reservations".*) as reservations'))
             .where({ 'Restaurants.id': id })
             .groupBy('Restaurants.id', 'Reservations.restaurantId');
+        // STEP 3: If Restaurant is not in the cache, add record to the cache
+
+        return restaurant;
+
     } catch (err) {
         console.error(err);
         throw new Error('Get Restaurant By ID -- DB Query')
--- src/routes/reservations.ts
+++ src/routes/reservations.ts
@@ -3,15 +3,18 @@ import { getReservationByIdController } from "../controllers/reservations/getRes
 import { deleteReservationController } from "../controllers/reservations/deleteReservation.controller";
 import { getReservationsController } from "../controllers/reservations/getReservations.controller";
 import { makeReservationController } from "../controllers/reservations/makeReservation.controller";
+import { updateReservationController } from "../controllers/reservations/updateReservation.controller";
 
 const getReservationsRoute = Router().get('/', getReservationsController);
 const getReservationByIdRoute = Router().get('/:reservationId', getReservationByIdController);
 const makeReservationRoute = Router().post('/', makeReservationController);
+const updateReservationRoute = Router().patch('/:reservationId', updateReservationController)
 const deleteReservationRoute = Router().delete('/:reservationId', deleteReservationController);
 
 export const reservationRouter = Router().use('/reservations', [
     getReservationsRoute,
     getReservationByIdRoute,
     makeReservationRoute,
+    updateReservationRoute,
     deleteReservationRoute
 ]);
