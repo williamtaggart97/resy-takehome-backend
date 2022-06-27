efff178b0657ed99b19f42c133d8f24d0b5656c9
Billy Taggart <williamtaggart97@gmail.com>
2022-06-27 11:23:06 -0400

    updated start script to include -a tag (needed if multiple apps in heroku account)

--- scripts/start-heroku.sh
+++ scripts/start-heroku.sh
@@ -10,13 +10,13 @@ if [ -z "$name" ]
         echo "No name argument supplied. Use -n flag to specify the name of your app"
     else
         heroku create $name
-        heroku addons:create heroku-postgresql:hobby-dev
+        heroku addons:create -a $name heroku-postgresql:hobby-dev
         # heroku addons:create heroku-redis:hobby-dev
         docker buildx build --platform linux/amd64 -t $name -f Dockerfile.prod .
         docker tag $name registry.heroku.com/$name/web
         docker push registry.heroku.com/$name/web  
         heroku container:release web -a $name
-        cat ./src/db/init-schema.sql | heroku pg:psql
-        cat ./src/db/seed-data.sql | heroku pg:psql
+        cat ./src/db/init-schema.sql | heroku pg:psql -a $name
+        cat ./src/db/seed-data.sql | heroku pg:psql -a $name
 fi
 
